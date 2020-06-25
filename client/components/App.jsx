902e4d1166c9770f03fd6d6b47ae5177835f5943
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-06-26 07:26:46 +1200

    title component added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import Home from './Home'
+import Title from './Title'
 
 const App = () => {
   return (
     <>
+      <Title />
       <Home />
       <h1>React development has begun!</h1>
     </>
