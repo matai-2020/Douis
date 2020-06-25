14ab47ee86045808bcb6bdd3f5815c207ffe476b
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-06-26 10:04:59 +1200

    Api getData function created, Home.jsx turned into class

---
+++ client/api.js
@@ -0,0 +1,11 @@
+import request from 'superagent'
+
+const dataUrl = 'http://localhost:3000/api/v1'
+
+export function getData () {
+  return request
+    .get(dataUrl)
+    .then(response => {
+      console.log('client-side: ', response.body)
+    })
+}
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import View from './View'
+import { getData } from '../api'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Area for image links</h1>
-      <Link to={'/restaurants'}>Link to Restaurant</Link>
-    </div>
-  )
+export class Home extends React.Component {
+    state = {
+      name: '',
+      image: ''
+    }
+
+    componentDidMount () {
+      getData()
+        .then(dbData => {
+          console.log('dbData on home page: ', dbData)
+        })
+    }
+
+    render () {
+      return (
+        <div>
+          <h1>Area for image links</h1>
+          <Link to={'/restaurants'}>Link to Restaurant</Link>
+        </div>
+      )
+    }
 }
 
 export default Home
