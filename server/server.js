e64f3aca27fd0cbf3a975609719c28d4550816ab
Ariana Mirus <63701400+ariana-mirus20@users.noreply.github.com>
2020-06-26 10:22:26 +1200

    structure for backend up and running

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const path = require('path')
 const express = require('express')
+const routes = require('./routes')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+server.use('/api/v1', routes)
 
 module.exports = server
