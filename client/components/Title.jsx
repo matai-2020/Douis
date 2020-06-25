4181e8b5bbf40047e5f6f8672263d971111a2749
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-06-26 08:49:39 +1200

    image added

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -4,6 +4,7 @@ const Title = () => {
   return (
     <div>
       <div className='title'>
+        <img className='dewey' src={'/images/dewey1.png'} />
         <h1>Douis' Top Picks</h1>
       </div>
     </div>
