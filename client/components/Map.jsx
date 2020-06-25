34cc6e22ed72d120ee4c140bc2facdf36de2b1b3
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-06-26 10:28:22 +1200

    add map

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,11 +1,45 @@
+import { Map as LeafletMap, LayersControl, TileLayer, Marker, Popup } from 'react-leaflet'
 import React from 'react'
 
-const Map = () => {
+const { BaseLayer, Overlay } = LayersControl
+
+const Map = (props) => {
+  const position = [-36.848461, 174.763336]
+  //   const position = [props.lat, props.long]
+  const zoom = 20
   return (
+
     <div>
       <h3>map</h3>
+
+      <div className="leaflet-container fontchange" id="mapid">
+
+        <LeafletMap center={position} zoom={zoom}>
+          <LayersControl position="topright">
+            <BaseLayer checked name="OpenStreetMap.HOT">
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+              />
+
+            </BaseLayer>
+            <BaseLayer name="OpenStreetMap.BlackAndWhite">
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+              />
+            </BaseLayer>
+            <Marker position={position}>
+              <Popup>
+              {/* {props.name} */}
+              </Popup>
+
+            </Marker>
+          </LayersControl>
+        </LeafletMap>
+
+      </div>
     </div>
   )
 }
-
 export default Map
